65e8181080846e4985ab7e6a9d8b6f91909a9d1e
Caio <caio.furtado@compasso.com.br>
2024-02-08 16:20:52 -0300

    first commit

---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: { default: 'dark' },
+    docs: {
+      theme: themes.dark,
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
