98307c49f839c9b6a9d9fea7ee8bfc717ac2d133
Caio <caio.furtado@compasso.com.br>
2024-02-15 13:55:06 -0300

    feature: add new addon and updates on package

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,42 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam odio quisquam cum rerum officia et ipsa amet illum illo reiciendis dolorum facere perspiciatis adipisci, laboriosam dicta blanditiis aliquid sit nisi.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: { type: 'select' },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text.',
+    as: 'strong',
+  },
+}
